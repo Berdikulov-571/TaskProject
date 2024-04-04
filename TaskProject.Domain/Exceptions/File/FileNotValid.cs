ebbc563f038a62dd0a46930639b0239cc693663d
berdikulov-571 <mygithub571@gmail.com>
2024-04-04 20:56:25 +0500

    Exception

---
+++ TaskProject.Domain/Exceptions/File/FileNotValid.cs
@@ -0,0 +1,12 @@
+﻿using static TaskProject.Domain.Exceptions.GlobalException;
+
+namespace TaskProject.Domain.Exceptions.File
+{
+    public class FileNotValid : GlobalException
+    {
+        public FileNotValid()
+        {
+            TitleMessage = "File Not Valid!";
+        }
+    }
+}
