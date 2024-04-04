b2159844d88359c61dae8eccb4e2b82a32228c25
berdikulov-571 <mygithub571@gmail.com>
2024-04-04 20:55:52 +0500

    Application Db Context

---
+++ TaskProject.DataAccess/Persistance/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskProject.Domain.Entities;
+using TaskProject.Service.Abstractions.DataAccess;
+
+namespace TaskProject.DataAccess.Persistance
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get ; set ; }
+    }
+}
