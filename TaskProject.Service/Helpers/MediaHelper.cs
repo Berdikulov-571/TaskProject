6a787a482daf3f9a6963517d5c911a1fdc95c931
berdikulov-571 <mygithub571@gmail.com>
2024-04-04 20:58:01 +0500

    Media Helpers

---
+++ TaskProject.Service/Helpers/MediaHelper.cs
@@ -0,0 +1,30 @@
+﻿using TaskProject.Domain.Exceptions.File;
+
+namespace TaskProject.Service.Helpers
+{
+    public class MediaHelper
+    {
+        public static string MakeFileName(string filename)
+        {
+            FileInfo fileInfo = new FileInfo(filename);
+
+            string[] ImageExtension = GetFileExtensions();
+
+            if (ImageExtension.Any(x => x == fileInfo.Extension))
+            {
+                string extension = fileInfo.Extension;
+                string name = "FILE_" + Guid.NewGuid() + extension;
+                return name;
+            }
+            throw new FileNotValid();
+        }
+
+        public static string[] GetFileExtensions()
+        {
+            return new string[]
+            {
+            ".mp4",
+            };
+        }
+    }
+}
