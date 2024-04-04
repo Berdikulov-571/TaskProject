17d32d451d92843e2fd08608de042504ebf8b6ec
berdikulov-571 <mygithub571@gmail.com>
2024-04-04 20:57:30 +0500

    Command Handler

---
+++ TaskProject.Service/UseCases/Products/Handlers/GetAllProductQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TaskProject.Domain.Entities;
+using TaskProject.Service.Abstractions.DataAccess;
+using TaskProject.Service.UseCases.Products.Queries;
+
+namespace TaskProject.Service.UseCases.Products.Handlers
+{
+    public class GetAllProductQueryHandler : IRequestHandler<GetAllProductQuery, IEnumerable<Product>>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public GetAllProductQueryHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> Handle(GetAllProductQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _context.Products.AsNoTracking().ToListAsync(cancellationToken);
+
+            return products;
+        }
+    }
+}
