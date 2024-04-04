bc41f7f43cdaea94fa4a3dfbaa2c16e13046da18
berdikulov-571 <mygithub571@gmail.com>
2024-04-04 20:58:45 +0500

    Dependecy Injection

---
+++ TaskProject.Service/DependecyInjection.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using TaskProject.Service.Abstractions.File;
+using TaskProject.Service.Services.Files;
+
+namespace TaskProject.Service
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddService(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddScoped<IFileService, FileService>();
+
+            return services;
+        }
+    }
+}
