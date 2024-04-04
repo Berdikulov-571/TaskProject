0e4aff57c63fbf20592ffd144946ed95f0be72e3
berdikulov-571 <mygithub571@gmail.com>
2024-04-04 20:57:54 +0500

    Command Handler

---
+++ TaskProject.Service/UseCases/Products/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TaskProject.Domain.Entities;
+
+namespace TaskProject.Service.UseCases.Products.Queries
+{
+    public class GetProductByIdQuery : IRequest<Product>
+    {
+        public int Id { get; set; }
+    }
+}
