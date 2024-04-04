5936ffb6f42be144826f7d624738c00b030fe4ed
berdikulov-571 <mygithub571@gmail.com>
2024-04-04 20:57:39 +0500

    Command Handler

---
+++ TaskProject.Service/UseCases/Products/Handlers/UpdateProductCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using TaskProject.Service.Abstractions.DataAccess;
+using TaskProject.Service.Abstractions.File;
+using TaskProject.Service.UseCases.Products.Commands;
+
+namespace TaskProject.Service.UseCases.Products.Handlers
+{
+    public class UpdateProductCommandHandler : IRequestHandler<UpdateProductCommand, int>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IFileService _fileService;
+
+        public UpdateProductCommandHandler(IApplicationDbContext context, IFileService fileService)
+        {
+            _context = context;
+            _fileService = fileService;
+        }
+
+        public async Task<int> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = await _context.Products.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+            if (product == null)
+                return 0;
+
+            if (request.Name != null)
+            {
+                product.Name = request.Name;
+            }
+            if(request.Description != null)
+            {
+                product.Description = request.Description;
+            }
+            if(request.SortNumber != null)
+            {
+                product.SortNumber = request.SortNumber;
+            }
+            if(request.VideoPath != null)
+            {
+                await _fileService.DeletFileAsync(product.VideoPath);
+                product.VideoPath = await _fileService.UploadFileAsync(request.VideoPath);
+            }
+
+            _context.Products.Update(product);
+            int response = await _context.SaveChangesAsync(cancellationToken);
+
+            return response;
+        }
+    }
+}
