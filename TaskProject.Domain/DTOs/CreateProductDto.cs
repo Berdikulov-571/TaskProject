fa22be564e0b3516230d706f5685b1881c61dc6a
berdikulov-571 <mygithub571@gmail.com>
2024-04-04 20:56:08 +0500

    Dtos

---
+++ TaskProject.Domain/DTOs/CreateProductDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TaskProject.Domain.DTOs
+{
+    public class CreateProductDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IFormFile VideoPath { get; set; }
+        public string SortNumber { get; set; }
+    }
+}
---
+++ TaskProject.Domain/DTOs/UpdateProductDto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TaskProject.Domain.DTOs
+{
+    public class UpdateProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public IFormFile? VideoPath { get; set; }
+        public string SortNumber { get; set; }
+    }
+}
