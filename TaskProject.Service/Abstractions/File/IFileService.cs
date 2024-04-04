95a2e5a1b20a6fcb915e39a0e491289dc904099c
berdikulov-571 <mygithub571@gmail.com>
2024-04-04 20:58:32 +0500

    IFileService

---
+++ TaskProject.Service/Abstractions/File/IFileService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace TaskProject.Service.Abstractions.File
+{
+    public interface IFileService
+    {
+        ValueTask<string> UploadFileAsync(IFormFile file);
+
+        ValueTask<bool> DeletFileAsync(string file);
+
+        ValueTask<byte[]> GetFileAsync(string path);
+    }
+}
