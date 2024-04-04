49ab3cc940f428d7981fa668bc2c27adf32a7eb7
berdikulov-571 <mygithub571@gmail.com>
2024-04-04 20:57:12 +0500

    Command

---
+++ TaskProject.Service/UseCases/Products/Commands/UpdateProductCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using TaskProject.Domain.DTOs;
+
+namespace TaskProject.Service.UseCases.Products.Commands
+{
+    public class UpdateProductCommand : UpdateProductDto, IRequest<int>
+    {
+
+    }
+}
