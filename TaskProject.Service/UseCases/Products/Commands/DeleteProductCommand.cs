1031aae6172adeec357f3a43b784397411cb2890
berdikulov-571 <mygithub571@gmail.com>
2024-04-04 20:57:06 +0500

    Command

---
+++ TaskProject.Service/UseCases/Products/Commands/DeleteProductCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace TaskProject.Service.UseCases.Products.Commands
+{
+    public class DeleteProductCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+    }
+}
