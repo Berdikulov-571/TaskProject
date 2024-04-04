81600a9d869c5edc0c86964d9cf3014495ab4dbf
berdikulov-571 <mygithub571@gmail.com>
2024-04-04 20:56:16 +0500

    Product

---
+++ TaskProject.Domain/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TaskProject.Domain.Entities
+{
+    public class Product
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string VideoPath { get; set; }
+        public string SortNumber { get; set; }
+    }
+}
