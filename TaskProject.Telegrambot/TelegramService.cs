3dfa6ebaed261eba7e6b63b90d27156e674b0449
berdikulov-571 <mygithub571@gmail.com>
2024-04-08 23:12:15 +0500

    Telegram Service

---
+++ TaskProject.Telegrambot/TelegramService.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.InputFiles;
+
+namespace TaskProject.Telegrambot
+{
+    public static ITelegramBotClient _botClient;
+
+    public static class TelegramService
+    {
+        public static async Task Main()
+        {
+            _botClient = new TelegramBotClient("6530285040:AAFOZe4HLj89L-yHVkyJLKrtqO9H2PSpxx4");
+
+            _botClient.OnMessage += Bot_OnMessage;
+
+            _botClient.StartReceiving();
+
+            Console.ReadLine();
+
+            //using (HttpClient client = new HttpClient())
+            //{
+            //    var response = await client.GetByteArrayAsync(@"https://localhost:44369/media//videos//FILE_1dd5bf84-d1c9-4714-a594-791c96832dd3.mp4");
+
+            //    System.IO.File.WriteAllBytes("D:\\Teste\\test.mp4", response);
+            //}
+        }
+
+
+    }
+}
