3c707447ff4ebbae55a971f73c29157134b5f2cf
berdikulov-571 <mygithub571@gmail.com>
2024-04-09 06:40:10 +0500

    Program cs

--- TelegramBot/Program.cs
+++ TelegramBot/Program.cs
@@ -1,12 +1,8 @@
-﻿using MediatR;
-using System;
-using System.Text.Json;
+﻿using System.Text.Json;
 using TaskProject.Domain.Entities;
-using TaskProject.Service.UseCases.Products.Queries;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types.InputFiles;
-using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace TelegramBot
@@ -22,6 +18,7 @@ namespace TelegramBot
             _botClient = new TelegramBotClient("6530285040:AAFOZe4HLj89L-yHVkyJLKrtqO9H2PSpxx4");
 
             _botClient.OnMessage += Bot_OnMessage;
+
             _botClient.OnCallbackQuery += Bot_OnCallbackQuery;
 
             _botClient.StartReceiving();
@@ -37,17 +34,34 @@ namespace TelegramBot
             {
                 await _botClient.SendTextMessageAsync(
                 chatId: e.Message.Chat.Id,
-                text: "Choose an option:",
-                replyMarkup: await SendInlineKeyboard()
-            );
+                text: $"Assalomu aleykum {e.Message.Chat.FirstName}",
+                replyMarkup: await SendKeyboardButton());
+            }
+            else
+            {
+                var products = await GetProductsFromApiAsync();
 
+                var product = products.FirstOrDefault(x => x.SortNumber == text);
+
+                if (product != null)
+                {
+                    await _botClient.SendTextMessageAsync(e.Message.Chat.Id, "Sending video✅");
+
+                    using (var videoStream = System.IO.File.OpenRead("../../../../TaspProject/wwwroot/" + product.VideoPath))
+                    {
+                        var videoInputFile = new InputOnlineFile(videoStream, Path.GetFileName("../../../../TaspProject/wwwroot/" + product.VideoPath));
+                        var message = await _botClient.SendVideoAsync(e.Message.Chat.Id, videoInputFile,replyMarkup: await
+                            SendKeyboardButton());
+                    }
+
+                    await _botClient.DeleteMessageAsync(e.Message.Chat.Id, e.Message.MessageId);
+                }
+                else
+                {
+                    await _botClient.SendTextMessageAsync(e.Message.Chat.Id, "Video Not Found❌", replyMarkup: await SendKeyboardButton());
+                }
 
             }
-        }
-        public static string[] GetVideos()
-        {
-            string[] videos = Directory.GetFiles("../../../../TaspProject/wwwroot/media/videos", "FILE_1dd5bf84-d1c9-4714-a594-791c96832dd3.mp4");
-            return videos;
         }
 
         public static async Task<InlineKeyboardMarkup> SendInlineKeyboard()
@@ -71,6 +85,7 @@ namespace TelegramBot
             response.EnsureSuccessStatusCode();
 
             var responseBody = await response.Content.ReadAsStringAsync();
+
             var products = JsonSerializer.Deserialize<List<Product>>(responseBody, new JsonSerializerOptions
             {
                 PropertyNameCaseInsensitive = true
@@ -83,22 +98,94 @@ namespace TelegramBot
         {
             string callbackData = e.CallbackQuery.Data;
 
-            var products = await GetProductsFromApiAsync();
-
-            var product = products.FirstOrDefault(x => x.Id == int.Parse(callbackData));
-
-            using (var videoStream = System.IO.File.OpenRead("../../../../TaspProject/wwwroot/" + product.VideoPath))
+            if (!callbackData.StartsWith("page"))
             {
-                var videoInputFile = new InputOnlineFile(videoStream, Path.GetFileName("../../../../TaspProject/wwwroot/" + product.VideoPath));
-                var message = await _botClient.SendVideoAsync(e.CallbackQuery.Message.Chat.Id, videoInputFile);
-                // Optionally, you can handle the message response here
+                var products = await GetProductsFromApiAsync();
+
+                var product = products.FirstOrDefault(x => x.Id == int.Parse(callbackData));
+
+                await _botClient.SendTextMessageAsync(e.CallbackQuery.Message.Chat.Id, "Sending video✅");
+
+                using (var videoStream = System.IO.File.OpenRead("../../../../TaspProject/wwwroot/" + product.VideoPath))
+                {
+                    var videoInputFile = new InputOnlineFile(videoStream, Path.GetFileName("../../../../TaspProject/wwwroot/" + product.VideoPath));
+                    var message = await _botClient.SendVideoAsync(e.CallbackQuery.Message.Chat.Id, videoInputFile);
+                }
+            }
+        }
+
+        public static async Task<ReplyKeyboardMarkup> SendKeyboardButton()
+        {
+            var response = await GetProductsFromApiAsync();
+
+            var halfCount = response.Count / 2;
+            var firstHalf = response.Take(halfCount);
+            var secondHalf = response.Skip(halfCount);
+
+            var keyboardRows = new List<List<KeyboardButton>>();
+
+            for (int i = 0; i < halfCount; i++)
+            {
+                var row = new List<KeyboardButton>
+                {
+                    new KeyboardButton { Text = firstHalf.ElementAt(i).SortNumber},
+                    new KeyboardButton { Text = secondHalf.ElementAt(i).SortNumber }
+                };
+                keyboardRows.Add(row);
             }
 
+            if (response.Count % 2 != 0)
+            {
+                keyboardRows.Last().Add(new KeyboardButton { Text = secondHalf.Last().SortNumber });
+            }
 
-            //_botClient.SendTextMessageAsync(
-            //    chatId: e.CallbackQuery.Message.Chat.Id,
-            //    text: $"You clicked on: {callbackData}"
-            //);
+            var replyKeyboardMarkup = new ReplyKeyboardMarkup(keyboardRows.ToArray())
+            {
+                ResizeKeyboard = true
+            };
+
+            return replyKeyboardMarkup;
         }
+
+        public static async Task<InlineKeyboardMarkup> SendInlineKeyboard(int page = 1, int pageSize = 5)
+        {
+            try
+            {
+                var response = await GetProductsFromApiAsync();
+
+                // Paginate the products
+                var products = response.Skip((page - 1) * pageSize).Take(pageSize).ToList();
+
+                var inlineKeyboard = new List<List<InlineKeyboardButton>>();
+
+                foreach (var product in products)
+                {
+                    var row = new List<InlineKeyboardButton>();
+                    row.Add(new InlineKeyboardButton { Text = product.SortNumber, CallbackData = product.Id.ToString() });
+                    inlineKeyboard.Add(row);
+                }
+
+                // Add navigation buttons
+                var navigationButtons = new List<InlineKeyboardButton>();
+                if (page > 1)
+                {
+                    navigationButtons.Add(new InlineKeyboardButton { Text = "Previous", CallbackData = $"page={page - 1}" });
+                }
+                if (response.Count > page * pageSize)
+                {
+                    navigationButtons.Add(new InlineKeyboardButton { Text = "Next", CallbackData = $"page={page + 1}" });
+                }
+                inlineKeyboard.Add(navigationButtons);
+
+                return new InlineKeyboardMarkup(inlineKeyboard);
+            }
+            catch (Exception ex)
+            {
+                // Handle the exception (log, throw, etc.)
+                Console.WriteLine($"Error in SendInlineKeyboard: {ex.Message}");
+                throw;
+            }
+        }
+
     }
 }
